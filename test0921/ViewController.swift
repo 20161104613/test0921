2751b2ca6ea90349ec5364f47bd91e5e9cef4ffa
20161104613 <1772569458@qq.com>
2018-09-26 11:27:26 +0800

    1
    1

--- test0921/ViewController.swift
+++ test0921/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zhangmanyu: UIButton!
+    
+    @IBOutlet weak var test1: UITextField!
+    
+
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
