c23b51d314cdfc780c0c2180e9ed242b1c1e4670
20161104613 <1772569458@qq.com>
2018-10-10 11:53:58 +0800

    1010

--- test0921/ViewController.swift
+++ test0921/ViewController.swift
@@ -12,64 +12,100 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var zhangmanyu: UIButton!
     @IBOutlet weak var display: UITextField!
-    var number1:Double = 0
-    var number2:Double = 0
-    var result:Double = 0
-    
-    var point:Double = 0.0
-    
-    var fuhao:String?
-    
-
+    var re = 0
     @IBAction func one(_ sender: Any) {
-        display.text = display.text!+"1"
+        if re == 1{
+            display.text = "1"
+        }
+        else{
+            display.text = display.text!+"1"
+        }
     }
     
     @IBAction func two(_ sender: Any) {
-        display.text = display.text!+"2"
+        if re == 1{
+            display.text = "2"
+        }
+        else{
+            display.text = display.text!+"2"
+        }
     }
     
     @IBAction func three(_ sender: Any) {
-        display.text = display.text!+"3"
+        if re == 1{
+            display.text = "3"
+        }
+        else{
+            display.text = display.text!+"3"
+        }
     }
     
     @IBAction func four(_ sender: Any) {
-        display.text = display.text!+"4"
+        if re == 1{
+            display.text = "4"
+        }
+        else{
+            display.text = display.text!+"4"
+        }
     }
     
     @IBAction func five(_ sender: Any) {
-        display.text = display.text!+"5"
+        if re == 1{
+            display.text = "5"
+        }
+        else{
+            display.text = display.text!+"5"
+        }
     }
     
     @IBAction func six(_ sender: Any) {
-        display.text = display.text!+"6"
+        if re == 1{
+            display.text = "6"
+        }
+        else{
+            display.text = display.text!+"6"
+        }
     }
     
     @IBAction func seven(_ sender: Any) {
-        display.text = display.text!+"7"
+        if re == 1{
+            display.text = "7"
+        }
+        else{
+            display.text = display.text!+"7"
+        }
     }
     
     @IBAction func eight(_ sender: Any) {
-        display.text = display.text!+"8"
+        if re == 1{
+            display.text = "8"
+        }
+        else{
+            display.text = display.text!+"8"
+        }
     }
     
     @IBAction func nine(_ sender: Any) {
-        display.text = display.text!+"9"
+        if re == 1{
+            display.text = "9"
+        }
+        else{
+            display.text = display.text!+"9"
+        }
     }
     
     @IBAction func add(_ sender: Any) {
-        //display.text = display.text!+"1"
-        if number1 != 0{
-            number1 = number2
-            number2 = 0
-        }
-        point = 0.0
-        //operator = sender.
+        
         
     }
     
     @IBAction func zero(_ sender: Any) {
-        display.text = display.text!+"0"
+        if re == 1{
+            display.text = "0"
+        }
+        else{
+            display.text = display.text!+"0"
+        }
     }
     
     @IBAction func minus(_ sender: Any) {
@@ -86,6 +122,12 @@ class ViewController: UIViewController {
         //if (number1 != 0) && (number2 != 0) && (fuhao != ""){
             //switch
         //
+    }
+    
+    
+    @IBAction func dot(_ sender: Any) {
+        display.text = display.text! + "."
+        //judge = 1
     }
     
     @IBAction func ac(_ sender: Any) {
